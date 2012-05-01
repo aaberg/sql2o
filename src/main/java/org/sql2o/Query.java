0b3e87e28d850d13804e82c5dfa774655adee063
Lars Aaberg <lars@aaberg.cc>
2012-05-01 13:24:27 +0200

    fixes #7. Fixed exception when setting a date parameter with value NULL while using DB2 quirkMode.

--- src/main/java/org/sql2o/Query.java
+++ src/main/java/org/sql2o/Query.java
@@ -161,7 +161,7 @@ public class Query {
 
     public Query addParameter(String name, java.util.Date value){
         Date sqlDate = value == null ? null : new Date(value.getTime());
-        if (this.connection.getSql2o().quirksMode == QuirksMode.DB2){
+        if (sqlDate != null && this.connection.getSql2o().quirksMode == QuirksMode.DB2){
             // With the DB2 driver you can get an error if trying to put a date value into a timestamp column,
             // but of some reason it works if using setObject().
             return addParameter(name, (Object)sqlDate);
