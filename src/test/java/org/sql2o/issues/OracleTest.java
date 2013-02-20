402022c05db4495a4a290b474a53e5802bc02621
Lars Aaberg <lars.aaberg@nordea.com>
2013-02-20 16:59:25 +0100

    Fixes issue #8. Error when converting from oracle.sql.TIMESTAMP to a java date type.

---
+++ src/test/java/org/sql2o/issues/OracleTest.java
@@ -0,0 +1,62 @@
+package org.sql2o.issues;
+
+import oracle.jdbc.driver.OracleDriver;
+import oracle.sql.Datum;
+import oracle.sql.TIMESTAMPTZ;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import org.sql2o.Sql2o;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ac23513
+ * Date: 20.02.13
+ * Time: 14:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class OracleTest {
+
+    private Sql2o sql2o;
+
+    public OracleTest() {
+        try {
+            DriverManager.registerDriver(new OracleDriver());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        this.sql2o = new Sql2o("jdbc:oracle:thin:@Unix0628:1521:ORA03T", "nlivca", "nordea");
+    }
+
+    /**
+     * Issue #8
+     * Cannot convert type class oracle.sql.TIMESTAMP to java.util.Date
+     *
+     *
+     * Caused by: org.sql2o.converters.ConverterException: Cannot convert type class oracle.sql.TIMESTAMP to java.util.Date
+     * at org.sql2o.converters.DateConverter.convert(DateConverter.java:25)
+     * at org.sql2o.converters.DateConverter.convert(DateConverter.java:14)
+     * at org.sql2o.reflection.Pojo.setProperty(Pojo.java:84)
+     *
+     *
+     */
+    @Test
+    public void testForIssue8OracleTimestamps() {
+        String sql = "select CURRENT_TIMESTAMP from dual";
+
+        //new TIMESTAMPTZ().timestampValue()
+
+        Date dateVal = sql2o.createQuery(sql).executeScalar(Date.class);
+        DateTime dateTimeVal = sql2o.createQuery(sql).executeScalar(DateTime.class);
+
+        assertThat(new DateTime(dateVal).toLocalDate(), is(equalTo(new LocalDate())));
+        assertThat(dateTimeVal.toLocalDate(), is(equalTo(new LocalDate())));
+    }
+}
