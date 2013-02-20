ef6827bb207e3f04ef5fccc862bc295eb2049c25
Lars Aaberg <lars.aaberg@nordea.com>
2013-02-20 14:27:03 +0100

    Fixed issue with db2 drivers. With some configurations on DB server, the ResultSetMetaData.getColumnLabel() method, returns a description of the column from the database, and not the alias.

--- src/main/java/org/sql2o/data/TableFactory.java
+++ src/main/java/org/sql2o/data/TableFactory.java
@@ -1,5 +1,6 @@
 package org.sql2o.data;
 
+import org.sql2o.QuirksMode;
 import org.sql2o.Sql2oException;
 
 import java.sql.ResultSet;
@@ -15,10 +16,10 @@ import java.sql.SQLException;
  */
 public class TableFactory {
     
-    public static Table createTable(ResultSet rs, boolean isCaseSensitive){
+    public static Table createTable(ResultSet rs, boolean isCaseSensitive, QuirksMode quirksMode){
         Table table = new Table(isCaseSensitive);
         try {
-            applyMetadata(table, rs.getMetaData());
+            applyMetadata(table, rs.getMetaData(), quirksMode);
 
         } catch (SQLException e) {
             throw new Sql2oException("Error while reading metadata from database", e);
@@ -39,12 +40,17 @@ public class TableFactory {
         return table;
     }
     
-    private static void applyMetadata(Table table, ResultSetMetaData metadata) throws SQLException {
+    private static void applyMetadata(Table table, ResultSetMetaData metadata, QuirksMode quirksMode) throws SQLException {
         
         table.setName( metadata.getTableName(1) );
 
         for (int colIdx = 1; colIdx <= metadata.getColumnCount(); colIdx++){
-            String colName = metadata.getColumnLabel(colIdx);
+            String colName;
+            if (quirksMode == QuirksMode.DB2){
+                colName = metadata.getColumnName(colIdx);
+            } else {
+                colName = metadata.getColumnLabel(colIdx);
+            }
             String colType = metadata.getColumnTypeName(colIdx);
             table.columns().add(new Column(colName, colIdx - 1, colType));
 
