6b12ac15fe53e8b1866be2f173f9324fc1c236c3
Alden Quimby <aldenquimby@gmail.com>
2014-01-20 19:59:51 -0500

    allow custom enum converter

---
+++ src/main/java/org/sql2o/converters/DefaultEnumConverter.java
@@ -0,0 +1,28 @@
+package org.sql2o.converters;
+
+/**
+ * Used by sql2o to convert a value from the database into an {@link Enum}.
+ */
+public class DefaultEnumConverter implements EnumConverter
+{
+    private Class enumType;
+
+    public void setEnumType(Class clazz)
+    {
+        enumType = clazz;
+    }
+
+    public Enum convert(Object val) throws ConverterException {
+        if (val == null) return null;
+        try{
+            if (String.class.isAssignableFrom(val.getClass())){
+                return Enum.valueOf(enumType, val.toString());
+            } else if (Number.class.isAssignableFrom(val.getClass())){
+                return (Enum)enumType.getEnumConstants()[((Number)val).intValue()];
+            }
+        } catch (Throwable t) {
+            throw new ConverterException("Error converting value '" + val.toString() + "' to " + enumType.getName(), t);
+        }
+        throw new ConverterException("Cannot convert type '" + val.getClass().getName() + "' to an Enum");
+    }
+}
