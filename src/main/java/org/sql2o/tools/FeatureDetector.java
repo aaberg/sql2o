c27ed9c4523be44f27b54e4280fd44aff64ec09a
Lars Aaberg <lars@aaberg.cc>
2014-02-23 15:23:15 +0100

    Added caching of underscore to camelCase conversion, for improved performance.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # HEAD detached from 35fe5e3
    # You are currently editing a commit while rebasing branch 'feature-beginTransactio-as-autoClosable' on '35fe5e3'.
    #
    # Changes to be committed:
    #   (use "git reset HEAD^1 <file>..." to unstage)
    #
    #	modified:   pom.xml
    #	modified:   src/main/java/org/sql2o/Connection.java
    #	modified:   src/test/java/org/sql2o/Sql2oTest.java
    #

--- src/main/java/org/sql2o/tools/FeatureDetector.java
+++ src/main/java/org/sql2o/tools/FeatureDetector.java
@@ -12,12 +12,14 @@ public final class FeatureDetector {
 
     static {
         cachePojoMetaDataEnabled(true); // enabled by default
+        setCacheUnderscoreToCamelcaseEnabled(true); // enabled by default
     }
 
     private static Boolean jodaTimeAvailable;
     private static Boolean slf4jAvailable;
     private static Boolean oracleAvailable;
     private static boolean cachePojoMetaDataEnabled;
+    private static boolean cacheUnderscoreToCamelcaseEnabled;
 
     /**
      * @return {@code true} if Joda-Time is available, {@code false} otherwise.
@@ -61,5 +63,20 @@ public final class FeatureDetector {
      */
     public static void cachePojoMetaDataEnabled(boolean b) {
         cachePojoMetaDataEnabled = b;
+    }
+
+    /**
+     *
+     * @return {@code true} if caching of underscore to camelcase is enabled.
+     */
+    public static boolean isCacheUnderscoreToCamelcaseEnabled() {
+        return cacheUnderscoreToCamelcaseEnabled;
+    }
+
+    /**
+     * Turn caching of underscore to camelcase on or off.
+     */
+    public static void setCacheUnderscoreToCamelcaseEnabled(boolean cacheUnderscoreToCamelcaseEnabled) {
+        FeatureDetector.cacheUnderscoreToCamelcaseEnabled = cacheUnderscoreToCamelcaseEnabled;
     }
 }
