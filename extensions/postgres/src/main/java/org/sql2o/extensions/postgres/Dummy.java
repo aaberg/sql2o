3af455369d84a62b2960848427e589435a4fb31a
Lars Aaberg <lars@aaberg.cc>
2014-04-03 22:25:57 +0200

    new project structure for better extensions support.

---
+++ extensions/postgres/src/main/java/org/sql2o/extensions/postgres/Dummy.java
@@ -0,0 +1,9 @@
+package org.sql2o.extensions.postgres;
+
+/**
+ * Created by lars on 03.04.14.
+ *
+ * Created the class as a placeholder. Should be deleted when actual functionality is added.
+ */
+public class Dummy {
+}
