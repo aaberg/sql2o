0cf802d477bf481b425f8a00dd4608b6777baa89
dimzon@gmail.com <dimzon541@gmail.com>
2014-04-06 22:54:38 +0400

    abstract thread-static cache implementation just inherit and implement evaluate method

---
+++ core/src/main/java/org/sql2o/tools/AbstractCache.java
@@ -0,0 +1,73 @@
+package org.sql2o.tools;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * just inherit and implement evaluate
+ * User: dimzon
+ * Date: 4/6/14
+ * Time: 10:35 PM
+ */
+public abstract class AbstractCache<K,V,E> {
+    private final Map<K,V> map;
+    private final ReentrantReadWriteLock rrwl;
+    private final ReentrantReadWriteLock.ReadLock rl;
+    private final ReentrantReadWriteLock.WriteLock wl;
+    /***
+     * @param map - allows to define your own map implementation
+     */
+    public AbstractCache(Map<K, V> map) {
+        this.map = map;
+        rrwl = new ReentrantReadWriteLock();
+        rl = rrwl.readLock();
+        wl = rrwl.writeLock();
+    }
+
+    public AbstractCache(){
+        this(new HashMap<K, V>());
+    }
+
+    private ThreadLocal<Boolean> tl = new ThreadLocal<Boolean>(){
+        @Override
+        protected Boolean initialValue() {
+            return Boolean.FALSE;
+        }
+    };
+
+    public V get(K key,E param){
+        V value;
+        if(tl.get()){
+            // current thread has exclusive cache lock
+            value = map.get(key);
+            if(value==null){
+                value = evaluate(key, param);
+                map.put(key,value);
+            }
+            return value;
+        }
+
+        // let's take read lock first
+        rl.lock();
+        try {
+            value = map.get(key);
+        } finally {
+            rl.unlock();
+        }
+        if(value!=null) return value;
+        wl.lock();
+        tl.set(Boolean.TRUE);
+        try {
+            value = evaluate(key, param);
+            map.put(key,value);
+        } finally {
+            tl.set(Boolean.FALSE);
+            wl.unlock();
+        }
+        return value;
+    }
+
+    protected abstract V evaluate(K key, E param);
+
+}
