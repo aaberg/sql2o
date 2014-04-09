743d57e4385fadcc570b287390745f3f0a338698
Lars Aaberg <lars@aaberg.cc>
2014-04-09 22:47:47 +0200

    added samples folder and java-edge sample project. This project is to test features not availlable with java 1.5.

---
+++ samples/java-edge/src/main/java/org/sql2o/samples/javaedge/Dummy.java
@@ -0,0 +1,8 @@
+package org.sql2o.samples.javaedge;
+
+/**
+ * Created by lars on 09.04.14.
+ */
+public class Dummy {
+    // dummy class, just here so the folder is checked into git. See test classes for samples.
+}
