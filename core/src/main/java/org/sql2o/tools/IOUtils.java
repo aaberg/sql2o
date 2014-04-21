9897aa74d8abbb5a0b53400b6e97d7f466ae8918
dimzon@gmail.com <dimzon541@gmail.com>
2014-04-21 23:49:53 +0400

    StringConverter revisited

--- core/src/main/java/org/sql2o/tools/IOUtils.java
+++ core/src/main/java/org/sql2o/tools/IOUtils.java
@@ -3,13 +3,12 @@ package org.sql2o.tools;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 /**
- * Created with IntelliJ IDEA.
  * User: lars
  * Date: 6/14/13
  * Time: 12:02 AM
- * To change this template use File | Settings | File Templates.
  */
 public class IOUtils {
 
@@ -19,12 +18,22 @@ public class IOUtils {
     public static byte[] toByteArray(InputStream input) throws IOException {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
-        long count = 0;
-        int n = 0;
+        int n;
         while (EOF != (n = input.read(buffer))) {
             output.write(buffer, 0, n);
-            count += n;
         }
         return output.toByteArray();
     }
+
+    public static String toString(Reader input) throws IOException {
+        StringBuilder output = new StringBuilder();
+        char[] buffer = new char[DEFAULT_BUFFER_SIZE];
+        int n;
+        while (EOF != (n = input.read(buffer))) {
+            output.append(buffer, 0, n);
+        }
+        return output.toString();
+    }
+
+
 }
