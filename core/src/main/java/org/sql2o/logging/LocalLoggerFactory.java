f06b8f08db7ca2db5c7a1516ec933204b8356ad1
dimzon@gmail.com <dimzon541@gmail.com>
2014-05-01 00:17:32 +0400

    cleanup

--- core/src/main/java/org/sql2o/logging/LocalLoggerFactory.java
+++ core/src/main/java/org/sql2o/logging/LocalLoggerFactory.java
@@ -7,12 +7,13 @@ import org.sql2o.tools.FeatureDetector;
  * Created by lars on 2/9/14.
  */
 public class LocalLoggerFactory {
+    // this is thread-safe since static initializer is thread-safe
+    private static final boolean slf4jAvailable = FeatureDetector.isSlf4jAvailable();
 
     public static Logger getLogger(Class clazz) {
-        if (FeatureDetector.isSlf4jAvailable()) {
-            return new Slf4jLogger(LoggerFactory.getLogger(clazz));
-        }
+        return slf4jAvailable
+                ? new Slf4jLogger(LoggerFactory.getLogger(clazz))
+                : SysOutLogger.instance;
 
-        return SysOutLogger.instance;
     }
 }
