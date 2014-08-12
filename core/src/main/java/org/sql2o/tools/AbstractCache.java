ffb17c3c5ce1c437b1a646624c771faa252e9e22
Lars Aaberg <lars@aaberg.cc>
2014-08-12 22:08:36 +0200

    Simplified AbstractCache class by removing the use of ThreadLocal. This should fix #138

--- core/src/main/java/org/sql2o/tools/AbstractCache.java
+++ core/src/main/java/org/sql2o/tools/AbstractCache.java
@@ -2,6 +2,8 @@ package org.sql2o.tools;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
@@ -12,14 +14,14 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  */
 public abstract class AbstractCache<K,V,E> {
     private final Map<K,V> map;
-    private final ReentrantReadWriteLock.ReadLock rl;
-    private final ReentrantReadWriteLock.WriteLock wl;
+    private final Lock rl;
+    private final Lock wl;
     /***
      * @param map - allows to define your own map implementation
      */
     public AbstractCache(Map<K, V> map) {
         this.map = map;
-        ReentrantReadWriteLock rrwl = new ReentrantReadWriteLock();
+        ReadWriteLock rrwl = new ReentrantReadWriteLock();
         rl = rrwl.readLock();
         wl = rrwl.writeLock();
     }
@@ -28,44 +30,26 @@ public abstract class AbstractCache<K,V,E> {
         this(new HashMap<K, V>());
     }
 
-    private final ThreadLocal<Boolean> tl = new ThreadLocal<Boolean>(){
-        @Override
-        protected Boolean initialValue() {
-            return Boolean.FALSE;
-        }
-    };
-
     public V get(K key,E param){
         V value;
-        if(tl.get()){
-            // evaluate implementation can re-enter into the cache
-            // current thread has exclusive cache lock
-            value = map.get(key);
-            if(value==null){
-                value = evaluate(key, param);
-                map.put(key,value);
-            }
-            return value;
-        }
 
-        // let's take read lock first
-        rl.lock();
         try {
+            // let's take read lock first
+            rl.lock();
             value = map.get(key);
         } finally {
             rl.unlock();
         }
         if(value!=null) return value;
-        wl.lock();
-        tl.set(Boolean.TRUE);
+
         try {
+            wl.lock();
             value = map.get(key);
             if(value==null){
                 value = evaluate(key, param);
                 map.put(key,value);
             }
         } finally {
-            tl.set(Boolean.FALSE);
             wl.unlock();
         }
         return value;
