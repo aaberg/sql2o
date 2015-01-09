91c7f3bb343a054303c572f5cccb421d8c16a8bd
alvn <an@unsilo.com>
2015-01-09 15:47:19 +0100

    fix(data): Removed some seamingly unnecessary annotations and added getBoolean

--- core/src/main/java/org/sql2o/data/Row.java
+++ core/src/main/java/org/sql2o/data/Row.java
@@ -13,7 +13,6 @@ import static org.sql2o.converters.Convert.throwIfNull;
 /**
  * Represents a result set row.
  */
-@SuppressWarnings({"UnusedDeclaration", "RedundantTypeArguments"})
 public class Row {
     
     private final Object[] values;
@@ -45,8 +44,7 @@ public class Row {
         throw new Sql2oException(String.format("Column with name '%s' does not exist", columnName));
     }
 
-    @SuppressWarnings("unchecked")
-    public <V> V getObject(int columnIndex, Class clazz){
+    public <V> V getObject(int columnIndex, Class<V> clazz){
         try{
             return (V) throwIfNull(clazz, quirks.converterOf(clazz)).convert(getObject(columnIndex));
         } catch (ConverterException ex){
@@ -54,8 +52,7 @@ public class Row {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    public <V> V getObject(String columnName, Class clazz) {
+    public <V> V getObject(String columnName, Class<V> clazz) {
         try{
             return (V) throwIfNull(clazz, quirks.converterOf(clazz)).convert(getObject(columnName));
         } catch (ConverterException ex){
@@ -64,81 +61,88 @@ public class Row {
     }
 
     public BigDecimal getBigDecimal(int columnIndex){
-        return this.<BigDecimal>getObject(columnIndex, BigDecimal.class);
+        return this.getObject(columnIndex, BigDecimal.class);
     }
     
     public BigDecimal getBigDecimal(String columnName){
-        return this.<BigDecimal>getObject(columnName, BigDecimal.class);
+        return this.getObject(columnName, BigDecimal.class);
+    }
+    
+    public Boolean getBoolean(int columnIndex){
+        return this.getObject(columnIndex, Boolean.class);
+    }
+    
+    public Boolean getBoolean(String columnName){
+        return this.getObject(columnName, Boolean.class);
     }
     
     public Double getDouble(int columnIndex){
-        return this.<Double>getObject(columnIndex, Double.class);
+        return this.getObject(columnIndex, Double.class);
     }
     
     public Double getDouble(String columnName){
-        return this.<Double>getObject(columnName, Double.class);
+        return this.getObject(columnName, Double.class);
     }
     
     public Float getFloat(int columnIndex){
-        return this.<Float>getObject(columnIndex, Float.class);
+        return this.getObject(columnIndex, Float.class);
     }
     
     public Float getFloat(String columnName){
-        return this.<Float>getObject(columnName, Float.class);
+        return this.getObject(columnName, Float.class);
     }
     
     public Long getLong(int columnIndex){
-        return this.<Long>getObject(columnIndex, Long.class);
+        return this.getObject(columnIndex, Long.class);
     }
     
     public Long getLong(String columnName){
-        return this.<Long>getObject(columnName, Long.class);
+        return this.getObject(columnName, Long.class);
     }
     
     public Integer getInteger(int columnIndex){
-        return this.<Integer>getObject(columnIndex, Integer.class);
+        return this.getObject(columnIndex, Integer.class);
     }
     
     public Integer getInteger(String columnName){
-        return this.<Integer>getObject(columnName, Integer.class);
+        return this.getObject(columnName, Integer.class);
     }
     
     public Short getShort(int columnIndex){
-        return this.<Short>getObject(columnIndex, Short.class);
+        return this.getObject(columnIndex, Short.class);
     }
     
     public Short getShort(String columnName){
-        return this.<Short>getObject(columnName, Short.class);
+        return this.getObject(columnName, Short.class);
     }
     
     public Byte getByte(int columnIndex){
-        return this.<Byte>getObject(columnIndex, Byte.class);
+        return this.getObject(columnIndex, Byte.class);
     }
     
     public Byte getByte(String columnName){
-        return this.<Byte>getObject(columnName, Byte.class);
+        return this.getObject(columnName, Byte.class);
     }
     
     public Date getDate(int columnIndex){
-        return this.<Date>getObject(columnIndex, Date.class);
+        return this.getObject(columnIndex, Date.class);
     }
     
     public Date getDate(String columnName){
-        return this.<Date>getObject(columnName, Date.class);
+        return this.getObject(columnName, Date.class);
     }
 
     public String getString(int columnIndex){
-        return this.<String>getObject(columnIndex, String.class);
+        return this.getObject(columnIndex, String.class);
     }
     
     public String getString(String columnName){
-        return this.<String>getObject(columnName, String.class);
+        return this.getObject(columnName, String.class);
     }
 
     /**
      * View row as a simple map.
      */
-    @SuppressWarnings("NullableProblems")
     public Map<String, Object> asMap()
     {   final List<Object> listOfValues = asList(values);
         return new Map<String, Object>() {
