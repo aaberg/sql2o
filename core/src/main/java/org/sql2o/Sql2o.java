3696062b53582c300346ebc9ae5af61a0ca352da
Lars Aaberg <lars@aaberg.cc>
2015-09-01 08:59:44 +0200

    closes #200. Improved error handling when opening transactions

--- core/src/main/java/org/sql2o/Sql2o.java
+++ core/src/main/java/org/sql2o/Sql2o.java
@@ -247,11 +247,17 @@ public class Sql2o {
 
         Connection connection = new Connection(this, false);
 
+        boolean success = false;
         try {
             connection.getJdbcConnection().setAutoCommit(false);
             connection.getJdbcConnection().setTransactionIsolation(isolationLevel);
+            success = true;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new Sql2oException("Could not start the transaction - " + e.getMessage(), e);
+        } finally {
+            if (!success) {
+                connection.close();
+            }
         }
 
         return connection;
