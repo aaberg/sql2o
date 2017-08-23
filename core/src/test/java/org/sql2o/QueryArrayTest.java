77813272b0195188cab6c4bd82f92bd7143379d6
Sondre Eikanger Kvalø <zapodot@gmail.com>
2017-08-23 19:44:29 +0200

    Test for issue #280

---
+++ core/src/test/java/org/sql2o/QueryArrayTest.java
@@ -0,0 +1,50 @@
+package org.sql2o;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.zapodot.junit.db.EmbeddedDatabaseRule;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.zapodot.junit.db.EmbeddedDatabaseRule.CompatibilityMode.Oracle;
+
+/**
+ * @author zapodot
+ */
+public class QueryArrayTest {
+
+    private static class Foo {
+        public int bar;
+    }
+    
+    @Rule
+    public EmbeddedDatabaseRule databaseRule = EmbeddedDatabaseRule.builder()
+            .withMode(Oracle)
+            .withInitialSql("CREATE TABLE FOO(BAR int PRIMARY KEY); INSERT INTO FOO VALUES(1); INSERT INTO FOO VALUES(2)")
+            .build();
+
+    @Test
+    public void arrayTest() throws Exception {
+        final Sql2o database = new Sql2o(databaseRule.getDataSource());
+        try(final Connection connection = database.open();
+            final Query query = connection.createQuery("SELECT * FROM FOO WHERE BAR IN (:bars)")) {
+            final List<Foo> foos = query.addParameter("bars", 1, 2).executeAndFetch(Foo.class);
+            assertThat(foos.size(), equalTo(2));
+
+        }
+    }
+
+    @Test
+    public void emptyArrayTest() throws Exception {
+        final Sql2o database = new Sql2o(databaseRule.getDataSource());
+
+        try(final Connection connection = database.open();
+            final Query query = connection.createQuery("SELECT * FROM FOO WHERE BAR IN (:bars)")) {
+
+            final List<Foo> noFoos = query.addParameter("bars", new Integer[]{}).executeAndFetch(Foo.class);
+            assertThat(noFoos.size(), equalTo(0));
+        }
+    }
+}
