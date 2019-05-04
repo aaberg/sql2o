cbf97e4c0240494aa4337118db266ba1dea715eb
Marco Sulla <github@marco.sulla.e4ward.com>
2019-05-04 02:48:34 +0200

    restored deleted files by error

---
+++ core/src/main/java/org/sql2o/converters/BigDecimalConverter.java
@@ -0,0 +1,36 @@
+package org.sql2o.converters;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * Used by sql2o to convert a value from the database into a {@link BigDecimal}.
+ */
+public class BigDecimalConverter extends NumberConverter<BigDecimal> {
+    public BigDecimalConverter() {
+        super(false);
+    }
+
+    @Override
+    protected BigDecimal convertNumberValue(Number val) {
+        if (val instanceof BigDecimal) {
+            return (BigDecimal) val;
+        }
+        if (val instanceof BigInteger) {
+            return new BigDecimal((BigInteger) val);
+        }
+        else {
+            return new BigDecimal(val.doubleValue());
+        }
+    }
+
+    @Override
+    protected BigDecimal convertStringValue(String val) {
+        return new BigDecimal(val);
+    }
+
+    @Override
+    protected String getTypeDescription() {
+        return BigDecimal.class.toString();
+    }
+}
