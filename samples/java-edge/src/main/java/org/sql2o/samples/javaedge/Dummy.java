742cf6804d32c3d2e37fad15dcb84f957b2d8fde
Lars Aaberg <lars@aaberg.cc>
2024-03-09 12:42:58 +0100

    Removed old and outdated samples folder

--- samples/java-edge/src/main/java/org/sql2o/samples/javaedge/Dummy.java
+++
@@ -1,8 +0,0 @@
-package org.sql2o.samples.javaedge;
-
-/**
- * Created by lars on 09.04.14.
- */
-public class Dummy {
-    // dummy class, just here so the folder is checked into git. See test classes for samples.
-}
